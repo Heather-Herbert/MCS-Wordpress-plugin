77fba6c3c523ea52461d0c57d185cab6b5cd36f5
Heather Herbert <Heather.Herbert.1975@gmail.com>
2018-09-23 18:33:51 +0100

    Added code to fix duplicate adding of links

--- MCScode.php
+++ MCScode.php
@@ -2,88 +2,98 @@
 
 class MCSCode {
 
-	private $mcs_settings_options;
+    private $mcs_settings_options;
 
-	public function __construct() {
-		$this->mcs_settings_options = get_option( 'mcs_settings_option_name' );
-		add_action( 'save_post', array( $this, 'mcs_function' ) );
-	}
-
-
-	function CallMCS ($host, $path, $key, $data) {
-
-		$headers = "Content-type: text/json\r\n" .
-        		"Ocp-Apim-Subscription-Key: $key\r\n";
-
-    		$data = json_encode ($data);
-		$options = array (
-			'http' => array (
-        		'header' => $headers,
-        		'method' => 'POST',
-        		'content' => $data
-        		)
-    		);
-		$context = stream_context_create ($options);
-		$result = file_get_contents ($host . $path, false, $context);
-		return $result;
-	}
+    public function __construct() {
+        $this->mcs_settings_options = get_option( 'mcs_settings_option_name' );
+        add_action( 'save_post', array( $this, 'mcs_function' ) );
+    }
 
 
-	function mcs_function( $post_id ){
-		$host = 'https://'.$this->mcs_settings_options['supported_hosts_0'].'.api.cognitive.microsoft.com';
-		
-		$accessKey = $this->mcs_settings_options['subscription_key_1'];
-		if (strlen($accessKey) > 0) {
-			if ( ! wp_is_post_revision( $post_id ) ){
+    function CallMCS ($host, $path, $key, $data) {
 
-				remove_action('save_post', array( $this, 'mcs_function' ));
-			
-				$post = get_post( $post_id );
-				$Text = strip_tags ($post->post_content);
-				$Text = html_entity_decode($Text);
-				$postData = substr ($Text,0,5000);
+        $headers = "Content-type: text/json\r\n" .
+            "Ocp-Apim-Subscription-Key: $key\r\n";
 
-				if ($this->mcs_settings_options['entities_2']) {
+        $data = json_encode ($data);
+        $options = array (
+            'http' => array (
+                'header' => $headers,
+                'method' => 'POST',
+                'content' => $data
+            )
+        );
+        $context = stream_context_create ($options);
+        $result = file_get_contents ($host . $path, false, $context);
+        return $result;
+    }
 
-					$keyPhrasesPath = '/text/analytics/v2.0/keyPhrases';
-					$data = array (
-					    'documents' => array (
-					        array ( 'id' => '1', 'language' => 'en', 'text' => $postData)
-    					    )
-					);
 
-					$KeyPhraseJSON = $this->CallMCS($host, $keyPhrasesPath, $accessKey, $data);
-					$result2Data = json_decode($KeyPhraseJSON);
-					$KeyPhraseArray = $result2Data->documents[0]->keyPhrases;
-					foreach ($KeyPhraseArray as $value) {
-    						$itemArray = $itemArray . $value . ', ';
-					}
-					wp_set_post_tags( $post_id , $itemArray, false );
-				}
-				
-				if ($this->mcs_settings_options['keyphrases_3']) {
+    function mcs_function( $post_id ){
+        $host = 'https://'.$this->mcs_settings_options['supported_hosts_0'].'.api.cognitive.microsoft.com';
 
-					$keyPhrasesPath = '/text/analytics/v2.0/entities';
-					$data = array (
-					    'documents' => array (
-					        array ( 'id' => '1', 'language' => 'en', 'text' => $postData)
-    					    )
-					);
+        $accessKey = $this->mcs_settings_options['subscription_key_1'];
+        if (strlen($accessKey) > 0) {
+            if ( ! wp_is_post_revision( $post_id ) ){
 
-					$KeyPhraseJSON = $this->CallMCS($host, $keyPhrasesPath, $accessKey, $data);
-					$result2Data = json_decode($KeyPhraseJSON);
-					$KeyPhraseArray = $result2Data->documents[0]->entities;
-					foreach ($KeyPhraseArray as $value) {
-						 $post->post_content = str_replace (  $value->name , "<a href='".$value->wikipediaUrl ."'>".$value->name ."</a>", $post->post_content);
-					}
-				}
+                remove_action('save_post', array( $this, 'mcs_function' ));
 
-				wp_update_post( $post );
+                $post = get_post( $post_id );
+                $Text = strip_tags ($post->post_content);
+                $Text = html_entity_decode($Text);
+                $postData = substr ($Text,0,5000);
 
-				add_action('save_post', array( $this, 'mcs_function' ));
-			}
-		}
-	}
+                if ($this->mcs_settings_options['entities_2']) {
+                    if (strpos($post->post_content, "<div id='MCS-keyPhrases'></div>") !== true) {
+
+                        $post->post_content = $post->post_content . "<div id='MCS-keyPhrases'></div>";
+
+                        $keyPhrasesPath = '/text/analytics/v2.0/keyPhrases';
+                        $data = array (
+                            'documents' => array (
+                                array ( 'id' => '1', 'language' => 'en', 'text' => $postData)
+                            )
+                        );
+
+                        $KeyPhraseJSON = $this->CallMCS($host, $keyPhrasesPath, $accessKey, $data);
+                        $result2Data = json_decode($KeyPhraseJSON);
+                        $KeyPhraseArray = $result2Data->documents[0]->keyPhrases;
+                        foreach ($KeyPhraseArray as $value) {
+                            $itemArray = $itemArray . $value . ', ';
+                        }
+                        wp_set_post_tags( $post_id , $itemArray, false );
+                    }
+                }
+
+                if ($this->mcs_settings_options['keyphrases_3']) {
+                    if (strpos($post->post_content, "<div id='MCS-entities'></div>") !== true) {
+
+                        $post->post_content = $post->post_content . "<div id='MCS-entities'></div>";
+
+                        $keyPhrasesPath = '/text/analytics/v2.0/entities';
+                        $data = array (
+                            'documents' => array (
+                                array ( 'id' => '1', 'language' => 'en', 'text' => $postData)
+                            )
+                        );
+
+                        $KeyPhraseJSON = $this->CallMCS($host, $keyPhrasesPath, $accessKey, $data);
+                        $result2Data = json_decode($KeyPhraseJSON);
+
+                        $KeyPhraseArray = $result2Data->documents[0]->entities;
+
+                        foreach ($KeyPhraseArray as $value) {
+                            $post->post_content = str_replace (  $value->name , "<a href='".$value->wikipediaUrl ."'>".$value->name ."</a>", $post->post_content);
+                        }
+                    }
+                }
+
+                wp_update_post( $post );
+
+                add_action('save_post', array( $this, 'mcs_function' ));
+            }
+        }
+    }
 
 }
 
